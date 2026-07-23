aec234bbf13c54fab47c4985480aff2131f5aa55
Tiyani <mhlarhit@gmail.com>
2026-07-23 17:34:01 +0200

    Added prefabs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,16 +6,27 @@ public class Bullet : MonoBehaviour
     private float _speed;
     private Vector2 _direction;
     private const float LifeTime = 5;
-    
+    private int _enemyCount;
+    //private int _bounce;
     void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         rb.linearDamping = 0;
         rb.gravityScale = 0;
         rb.linearVelocity = transform.right * PlayerController.Instance.bulletSpeed;
+        _enemyCount = 2;
+        //_bounce = 2;
         Destroy(gameObject, LifeTime);
     }
-    
+
+    public void EnemyHit()
+    {
+        _enemyCount--;
+        if (_enemyCount<=0)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void FixedUpdate()
     {
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -24,7 +24,7 @@ public class Enemy : MonoBehaviour
     {
         if (other.CompareTag("Bullet"))
         {
-            Destroy(other.gameObject);
+            other.GetComponent<Bullet>().EnemyHit();
             Hit();
         }
     }
