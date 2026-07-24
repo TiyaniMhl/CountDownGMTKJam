bbe016eeb13785cbb674e0110374a45706fd2f31
Tiyani <mhlarhit@gmail.com>
2026-07-25 01:07:39 +0200

    UI cleanup

--- Assets/Scripts/LevelUI.cs
+++ Assets/Scripts/LevelUI.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
 public class LevelUI : MonoBehaviour
 {
     public TextMeshProUGUI bulletCountText;
+    public TextMeshProUGUI countFlash;
     public GameObject thirdStar;
     public GameObject secondStar;
 
@@ -26,7 +28,8 @@ public class LevelUI : MonoBehaviour
 
     void Start()
     {
-        
+        countFlash.alpha = 0f;
+        bulletCountText.text = GameController.Instance.bullets + "";
     }
 
     public void ThreeStars()
@@ -48,8 +51,28 @@ public class LevelUI : MonoBehaviour
     public void UpdateBullets(int b)
     {
         bulletCountText.text = b + "";
+        countFlash.text = b + "";
+        StartCoroutine(CountFlash());
     }
-    
+
+    public IEnumerator CountFlash()
+    {
+        countFlash.alpha = 1f;
+        countFlash.fontSize = bulletCountText.fontSize + 10f;
+
+        const float duration = 2f;
+        float t = 0f;
+
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            countFlash.alpha = Mathf.Lerp(1f, 0f, t / duration);
+            yield return null;
+        }
+
+        countFlash.alpha = 0f;
+    }
+
 
     // Update is called once per frame
     void Update()
